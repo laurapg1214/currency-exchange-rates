1276050f0e4380ae4cf46fdd7811e3c9ecc9b16c
Laura Purcell-Gates <laurapg1214@users.noreply.github.com>
2024-11-11 18:06:39 +0000

    Currency Converter component created

--- src/CurrencyConverter.js
+++ src/CurrencyConverter.js
@@ -1,76 +1,214 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import getEmojiByCurrencyCode from 'country-currency-emoji-flags';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { json, checkStatus } from './utils';
 
-// add in below (transferred from ExchangeRates)
-this.handleChange = this.handleChange.bind(this);
-this.handleSubmit = this.handleSubmit.bind(this);
-}
+const CurrencyConverterTable = (props) => {
+  // props object destructuring 
+  const { 
+    from, 
+    to, 
+    amount, 
+    convertedAmount, 
+    rates,
+    handleAmountChange,
+    handleFromSelection,
+    handleToSelection
+  } = props;
 
-handleChange(event) {
-// update amount as user types
-this.setState({ amount: event.target.value });
-}
+  // return Currency Converter table
+  return (
+    <div className="container d-flex flex-column justify-content-center align-items-center min-vh-100">
+      <h3 className="text-center mb-4">Currency Converter</h3>
 
-handleSubmit(event) {
-event.preventDefault();
-let { amount } = this.state;
-amount = parseFloat(amount);
-if (!amount) {
-    return;
-};
+      {/* base currency dropdown */}
+      <div className="form-group">
+        <label htmlFor="baseCurrency" className="form-label">Base Currency</label>
+        <select 
+          id="baseCurrency" 
+          className="form-select" 
+          value={from}
+          onChange={handleFromSelection}
+        >
+          {
+            Object.keys(rates).map((currencyCode) => {
+              /* get flag emojis
+              (from https://snyk.io/advisor/npm-package/country-currency-emoji-flags) */
+              const flag = getEmojiByCurrencyCode(currencyCode);
+              return (
+                // key for React rendering, value as what's submitted/used when user selects
+                <option key={currencyCode} value={currencyCode}>
+                  {/* render flag emoji with currency code */}
+                  {flag} {currencyCode}
+                </option>
+              );
+            })
+          }
+        </select>
+      </div>
+
+      {/* double arrow between currencies from https://www.w3schools.com/charsets/ref_utf_arrows.asp */}
+      <span className="arrow">&#8596;</span>
+
+      {/* target currency dropdown */}
+      <div className="form-group">
+        <label htmlFor="target-currency" className="form-label">Target Currency</label>
+        <select 
+          id="targetCurrency" 
+          className="form-select" 
+          value={to}
+          onChange={handleToSelection}
+        >
+          {
+            Object.keys(rates).map((currencyCode) => {
+              /* get flag emojis
+              (from https://snyk.io/advisor/npm-package/country-currency-emoji-flags) */
+              const flag = getEmojiByCurrencyCode(currencyCode);
+              return (
+                <option key={currencyCode} value={currencyCode}>
+                  {/* render flag emoji with currency code */}
+                  {flag} {currencyCode}
+                </option>
+              );
+            })
+          }
+        </select>
+      </div>
+
+      {/* amount input & converted amount fields */}
+      <div className="amount-container mt-4">
+        <div className="form-group">
+          <label htmlFor="amount" className="form-label">Amount</label>
+          <input 
+            type="number" 
+            id="amount" 
+            className="form-control" 
+            value={amount} 
+            onChange={handleAmountChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="convertedAmount">Converted Amount</label>
+          <input 
+            type="text" 
+            id="convertedAmount" 
+            value={convertedAmount} 
+            readOnly
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
 
 class CurrencyConverter extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        amount: 0.0,
-        base: '',
-        date: '',
-        rates: {},
-        error: '',
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      from: 'EUR',
+      to: 'USD',
+      amount: 0.0,
+      convertedAmount: 0.0,
+      rates: {},
+      error: '',
+    };
+
+    this.handleAmountChange = this.handleAmountChange.bind(this);
+    this.handleFromSelection = this.handleFromSelection.bind(this);
+    this.handleToSelection = this.handleToSelection.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.convert = this.convert.bind(this);
+  }
+
+  componentDidMount() {
+    // fetch current rates for dropdown lists
+    fetch(`https://api.frankfurter.app/latest?base=EUR`)
+      .then(checkStatus)
+      .then(json)
+      .then((data) => {
+        // update state based on data from api
+        this.setState({
+          rates: data.rates
+        });
+      })
+      // error handling
+      .catch((error) => {
+        this.setState({
+          error: error.message || 'Error while fetching data'
+        })
+      });
+  }
   
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+  handleAmountChange(event) {
+    // update amount as user types
+    this.setState({ amount: parseFloat(event.target.value) });
+  }
+
+  // listener for base currency selected from dropdown
+  handleFromSelection(event) {
+    // update base currency from selection
+    this.setState({ from: event.target.value })
+  }
+
+  handleToSelection(event) {
+    // update target currency from selection
+    this.setState({ to: event.target.value })
+  }
+
+  // conversion function adapted from frankfurter.dev incl fetch api call
+  convert(from, to, amount) {
+    // return if no amount selected
+    if (amount === 0) {
+      alert('Please select an amount to convert');
+      return;
     }
+
+    // api call 
+    fetch(`https://api.frankfurter.app/latest?base=${from}&symbols=${to}`)
+      .then(checkStatus)
+      .then(json)
+      .then((data) => {
+        // run conversion algorithm
+        const convertedAmount = (amount * data.rates[to]).toFixed(2);
+        // update convertedAmount state using shorthand (object key & variable name identical)
+        this.setState({ convertedAmount });
+      })
+      .catch((error) => {
+        this.setState({
+          error: error.message || 'Error while fetching data'
+        })
+      });
+  }
   
-    handleChange(event) {
-        // update amount as user types
-        this.setState({ amount: event.target.value });
-    }
+  handleSubmit(event) {
+    event.preventDefault();
+    let { from, to, amount } = this.state;
+    this.convert(from, to, amount);
+  }
+    
   
-    handleSubmit(event) {
-        event.preventDefault();
-        let { amount } = this.state;
-        amount = amount.parseFloat();
-        if (!amount) {
-            return;
-        }
-        
-        // fetch api call to frankfurter.app
-    }
-  
-    render() {
-      const { base, date, rates, error } = this.state;
-  
+  render() {
+    const { from, to, amount, convertedAmount, rates, error } = this.state;
+    // if error, render error message in DOM
+    if (error) {
       return (
-        <div className="container">
-          <div className="text-center p-3 mb-2">
-            <h2 className="mb-2">Currency Converter</h2>
-            <h4>USD 1: {rate} EURO</h4>
-          </div>
-          <div className="row text-center">
-            <div className="col-12">
-              <span className="mr-1">USD</span>
-              <input value={usd} onChange={this.handleUsdChange} type="number" />
-              <span className="mx-3">=</span>
-              <input value={euro} onChange={this.handleEuroChange} type="number" />
-              <span className="ml-1">EURO</span>
-            </div>
-          </div>
-        </div>
+        <p className="text-danger">{error}</p>
       )
     }
+    // call child component CurrencyConverterTable for rendering in DOM
+    return (
+      <div className="container">
+        <CurrencyConverterTable 
+          from={from} 
+          to={to} 
+          amount={amount}
+          convertedAmount={convertedAmount}
+          rates={rates} 
+        />
+      </div>
+    )
+  }
 }
   
   const container = document.getElementById('root');
